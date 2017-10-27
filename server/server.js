fc5dbcf108ab0bad2e478ec0a20bb344007e89ff
Marta Jopp <martapeterson@gmail.com>
2017-10-27 16:03:51 -0500

    server up and running

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = 5000;
+
+// Serve up static files
+app.use(express.static('server/public'));
+// Start up the server
+app.listen(port, function () {
+    console.log('listening on port', port);
+});
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // Used for jQuery
