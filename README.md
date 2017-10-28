19d97a7265754b1781eb588f8c80ed8e0a9da92d
Marta Jopp <martapeterson@gmail.com>
2017-10-27 19:02:58 -0500

    skeleton of basebase mode

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# gallery-my-life
+## Facebook for Myself
+
+The Gallery of My Life:
+
+This application shows pictures of significance to me.  When clicking upon a photo you can read a brief caption of why I chose to display the photo.  Users can also star my photos as many times as they would like - and the number of stars is shown.
+
+This application is also available on heroku.
+
+#Technologies Used:
+
+--Node
+--Express
+--Angular
+--CSS
+--Heroku
+--JavaScript
+
+
+
+
+
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,30 @@ var myApp = angular.module('myApp', []);
 myApp.controller('myLifeController', function (){
     var life = this;
     life.message = 'Hello life.message';
+
+    life.pictures = [{
+        photo: '/pictures/family.jpg',
+        caption: '"caption about picture"',
+        likes: 0,
+        photoShow: true,
+        captionShow: false,
+    },
+{ photo: '/pictures/newborn.JPG',
+caption: '"Dylan newborn"',
+likes: 0,
+photoShow: true,
+captionShow: false,
+}]
+
+    life.clip = function (photo) {
+        console.log(photo);
+        photo.photoShow = !photo.photoShow
+        photo.captionShow = !photo.captionShow
+};
+
+life.like = function (photo) {
+    photo.likes = photo.likes + 1;
+}
+
+
 });
